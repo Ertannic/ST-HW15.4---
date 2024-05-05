a2ea06e574874c110ba1dc355e2088fb6e9372d7
Erzhan <saralaev03@mail.ru>
2024-05-05 23:36:18 +0600

    Initial Commit

---
+++ ST-HW15.4 - Саралаев Ержан/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ST-HW15.4 - Саралаев Ержан
+//
+//  Created by Ertannic Saralay on 05.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
